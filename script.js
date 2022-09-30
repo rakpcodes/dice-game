63e8a6ab4fa2b745af9362ca39576858af377161
rakpcodes <rakpcodes@gmail.com>
2022-09-30 19:49:29 -0400

    stored elements in variables

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 'use strict';
 
+// Element Selectors
 
+const dice = document.querySelector('.dice')
+const player1 = document.querySelector('.player--1')
+const player2 = document.querySelector('.player--2')
+
+// Player Scores
+const score1 = document.querySelector('#score--1')
+const score2 = document.querySelector('#score--2')
+const currentBank1 = document.querySelector('#current--1')
+const currentBank2 = document.querySelector('#current--2')
+
+// Buttons
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
