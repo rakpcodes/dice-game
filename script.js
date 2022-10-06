85aaa8e1d18ce50b636e682cf6bf2acde3786190
rakpcodes <rakpcodes@gmail.com>
2022-10-06 17:25:23 -0400

    added toggle for active player and logic to change players current scores

--- script.js
+++ script.js
@@ -3,14 +3,15 @@
 // Element Selectors
 
 const dice = document.querySelector('.dice')
-const player1 = document.querySelector('.player--1')
-const player2 = document.querySelector('.player--2')
+const player1 = document.querySelector('.player--0')
+const player2 = document.querySelector('.player--1')
+const playerActive = document.querySelector(".player--active")
 
 // Player Scores
+const score0 = document.querySelector('#score--0')
 const score1 = document.querySelector('#score--1')
-const score2 = document.querySelector('#score--2')
+const current0 = document.querySelector('#current--0')
 const current1 = document.querySelector('#current--1')
-const current2 = document.querySelector('#current--2')
 
 // Buttons
 const btnNew = document.querySelector('.btn--new')
@@ -18,17 +19,19 @@ const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
 // Starting Conditions
-let currentScore = 0
+score0.textContent = 0
 score1.textContent = 0
-score2.textContent = 0
 dice.classList.add('hidden')
 
+const scores = [0, 0]
+let currentScore = 0
+let activePlayer = 0
 
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
 
     // Generate Random Dice Roll
-    const randomNum = Math.floor(Math.random() * 6) + 1
+    const randomNum = Math.floor(Math.random() * 5) + 2
 
     // Display Dice
     dice.classList.remove('hidden')
@@ -37,7 +40,27 @@ btnRoll.addEventListener('click', function () {
     //Check for 1, if rolled, change players
 
     if (randomNum !== 1) {
-        currentScore += randomNum;
-        current1.textContent = currentScore
+        currentScore += randomNum; // increases the 'current' score
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore // selects and updates score for the active player
+    } else {
+        document.querySelector(`#current--${activePlayer}`).textContent = 0 // resets current score for new active player
+        currentScore = 0; // resets 'current' score for old active player
+        activePlayer = activePlayer === 0 ? 1 : 0; // decides which player is active
+        player1.classList.toggle('player--active')
+        player2.classList.toggle('player--active')
+
     }
+})
+
+btnHold.addEventListener('click', function () {
+
+
+    document.querySelector(`#current--${activePlayer}`).textContent = 0 // resets current score for new active player
+    currentScore = 0; // resets 'current' score for old active player
+    activePlayer = activePlayer === 0 ? 1 : 0; // decides which player is active
+
+    player1.classList.toggle('player--active')
+    player2.classList.toggle('player--active')
+
+
 })
